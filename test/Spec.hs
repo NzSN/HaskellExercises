0d5ab468bbf6190af2ae710db01e83415eebf461
Linshizhi <linshizhi@laihua.com>
2022-07-01 18:49:03 +0800

    add quickcheck

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,14 @@
+import Test.Hspec
+import Test.QuickCheck (quickCheck, forAll, elements, (==>))
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  spec_1
+
+spec_1 :: IO ()
+spec_1 = do
+  quickCheck $ prop_Insert 1 [1,2,3,4]
+  where
+    ordered xs = and (zipWith (<=) xs (drop 1 xs))
+    insert x xs = takeWhile (<x) xs ++ [x] ++ dropWhile (<x) xs
+    prop_Insert x xs = ordered xs ==> ordered (insert x xs)
